e105b474e8b5d68af47129dda52ae3839cead444
Aziz S <sellaziz.code@outlook.fr>
2024-05-24 19:21:22 +0200

    astronvim: update theme and languages
    Default theme to tokyonight
    Add cppp, rust

---
+++ config/nvim/lua/plugins/astrocommunity.lua
@@ -0,0 +1,6 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.rust" },
+}
